774963896993b0e67e9cc48f38477eac9623e79b
kwat0g <bungubung.ehdrian@ncst.edu.ph>
2026-03-09 21:31:05 +0800

    deploy: sync local changes — public/build/assets/APDueDateMonitorPage-D8QECCB9.js, public/build/assets/APInvoiceDetailPage-iBEL9AX4.js, public/build/assets/APInvoiceFormPage-CT_2H4hU.js, public/build/assets/APInvoicesPage-ByA4fUrC.js, public/build/assets/AccountsPage-8Y96gxAQ.js (+234 more)

---
+++ public/build/assets/usePermission-DUgMlKwz.js
@@ -0,0 +1,1 @@
+import{u as e}from"./main-DtDB7qKi.js";function u(s){return e(r=>r.hasPermission(s))}export{u};
--- public/build/assets/usePermission-DmqIhDYB.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./main-BJT3sR8t.js";function u(s){return e(r=>r.hasPermission(s))}export{u};
